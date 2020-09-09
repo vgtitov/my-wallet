47020493ff8a21828cb5640eca8bcead7423a410
vgtitov <v.titov.7@gmail.com>
2020-09-09 16:46:27 +0300

    Initial Commit

---
+++ my-wallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  my-wallet
+//
+//  Created by it10 on 09.09.2020.
+//  Copyright © 2020 Viachaslau Tsitou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
